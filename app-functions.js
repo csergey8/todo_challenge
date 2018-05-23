01fd7a752410ba721613c63eb50d0dc48d387938
csergey8 <c.sergey@protonmail.com>
2018-05-23 10:45:05 +0300

    refactor to some es6 features

--- app-functions.js
+++ app-functions.js
@@ -1,40 +1,66 @@
-const getSavedTodos = function() {
-    if(localStorage.getItem('todos') !== null) {
-        return JSON.parse(localStorage.getItem('todos'));
-    } else {
-        return [];
-    }
+const getSavedTodos = () => localStorage.getItem('todos') !== null ? JSON.parse(localStorage.getItem('todos')) : [];
+
+const removeTodo = id => {
+    const todoIndex = todos.findIndex(todo =>  todo.id === id);
+    console.log(todoIndex);
+
+    todoIndex > -1 ? todos.splice(todoIndex, 1) : null;
+
 }
 
-const saveTodos = function(todo) {
-    localStorage.setItem('todos', JSON.stringify(todo));
+const saveTodos = todo => localStorage.setItem('todos', JSON.stringify(todo));
+
+const toggleTodo = id => {
+    const todo = todos.find(todo => todo.id === id);
+
+    todo !== undefined ? todo.completed = !todo.completed : null;
 }
 
-const renderTodos = function(todos, filters) {
-    const filteredTodos = todos.filter(function(todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase()) && !filters.hideCompleted || !todo.completed;
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter(todo => {
+        const filterTodo = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        return  filterTodo && (!filters.hideCompleted || !todo.completed);
     });
-
-    const incompleteTodos = filteredTodos.filter(function(todo) {
-        return !todo.completed
-    });
+    const incompleteTodos = filteredTodos.filter(todo => !todo.completed);
 
     document.querySelector('#todos').innerHTML = '';
     document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos));
 
-    filteredTodos.forEach(function(todo) {
+    filteredTodos.forEach(todo => {
         const p = generateTodoDOM(todo);
         document.querySelector('#todos').appendChild(p);
     });
 };
 
-const generateTodoDOM = function(todo) {
-    const p = document.createElement('p');
-    p.textContent = todo.text;
-    return p
+const generateTodoDOM = todo => {
+    const divTodo = document.createElement('div');
+    const checkboxTodo = document.createElement('input');
+    const todoText = document.createElement('span');
+    const buttonDelete = document.createElement('button');
+  
+    checkboxTodo.setAttribute('type', 'checkbox'); 
+    checkboxTodo.checked = todo.completed;
+    divTodo.appendChild(checkboxTodo);
+    checkboxTodo.addEventListener('change', e => {
+        toggleTodo(todo.id);
+        saveTodos(todos);
+        renderTodos(todos, filters);
+    });
+    
+    todoText.textContent = todo.text;
+    divTodo.appendChild(todoText);
+
+    buttonDelete.textContent = 'x';
+    divTodo.appendChild(buttonDelete);
+    buttonDelete.addEventListener('click', e => {
+        removeTodo(todo.id);
+        saveTodos(todos);
+        renderTodos(todos, filters); 
+
+    return divTodo
 };
 
-const generateSummaryDOM = function(incompleteTodos) {
+const generateSummaryDOM = incompleteTodos => {
     const total = document.createElement('p');
     total.textContent = `Incompleted todos: ${incompleteTodos.length}`;
     return total
--- app.js
+++ app.js
@@ -5,32 +5,34 @@ const filters = {
     hideCompleted: false
 }
 
-const hideCompleted = function(todos) {
-    const uncompletedTodo = todos.filter(function(todo) {
-        return !todo.completed
-    });
+const hideCompleted = todos => {
+    const uncompletedTodo = todos.filter(todo => !todo.completed);
     renderTodos(uncompletedTodo, filters);
 }
 
-document.querySelector('#search-todo').addEventListener('input', function(e){
+document.querySelector('#search-todo').addEventListener('input', e => {
     filters.searchText = e.target.value; 
     renderTodos(todos, filters);
 });
 
-document.querySelector('#add-todo').addEventListener('submit', function(e){
+document.querySelector('#add-todo').addEventListener('submit', e => {
     e.preventDefault();
-    const newTodo = { text: e.target.elements.todo.value, completed: false };
+    const newTodo = {
+        id: uuidv4(), 
+        text: e.target.elements.todo.value, 
+        completed: false };
     todos.push(newTodo);
     saveTodos(todos);
     renderTodos(todos, filters);
     e.target.elements.todo.value = '';
 });
 
-document.querySelector('#completed').addEventListener('change', function(e) {
+document.querySelector('#completed').addEventListener('change', e => {
     filters.hideCompleted =  e.target.checked;
     renderTodos(todos, filters);
 });
-console.log(todos);
+
+
 renderTodos(todos, filters);
 
 
